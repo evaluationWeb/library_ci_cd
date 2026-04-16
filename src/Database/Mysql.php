e61536081771ffc5cba72612ab28273b8babb997
mithridatem <mathieumithridate@adrar-formation.com>
2026-04-16 09:52:41 +0200

    fix: add security recommendation in Mysql Class

--- src/Database/Mysql.php
+++ src/Database/Mysql.php
@@ -8,10 +8,14 @@ class Mysql
     {
         //Création d'un objet PDO
         return new \PDO(
-            'mysql:host=' . $_ENV["DATABASE_HOST"] . ';dbname=' . $_ENV["DATABASE_NAME"] . '',
+            'mysql:host=' . $_ENV["DATABASE_HOST"] . ';dbname=' . $_ENV["DATABASE_NAME"] . ';charset=utf8mb4',
             $_ENV["DATABASE_USERNAME"],
             $_ENV["DATABASE_PASSWORD"],
-            [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]
+            [
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+                \PDO::ATTR_EMULATE_PREPARES => false,
+            ]
         );
     }
 }
